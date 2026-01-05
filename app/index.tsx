9ffa85b50d58456864898f1e71084e255e4502c3
Oliver Henrichs <ohenrichs@gmail.com>
2026-01-05 21:53:17 +0100

    re-adds the safeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -3,21 +3,24 @@ import PatternListManager from "@/components/pattern/PatternListManager";
 import SettingsScreen from "@/components/pattern/SettingsScreen";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Drawer = createDrawerNavigator();
 
 export default function Index() {
   return (
-    <Drawer.Navigator
-      initialRouteName="Patterns"
-      screenOptions={{
-        drawerPosition: "right",
-        headerShown: false,
-        swipeEdgeWidth: 40,
-      }}
-    >
-      <Drawer.Screen name="Patterns" component={PatternListManager} />
-      <Drawer.Screen name="Settings" component={SettingsScreen} />
-    </Drawer.Navigator>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f5f3ff" }}>
+      <Drawer.Navigator
+        initialRouteName="Patterns"
+        screenOptions={{
+          drawerPosition: "right",
+          headerShown: false,
+          swipeEdgeWidth: 40,
+        }}
+      >
+        <Drawer.Screen name="Patterns" component={PatternListManager} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+      </Drawer.Navigator>
+    </SafeAreaView>
   );
 }
