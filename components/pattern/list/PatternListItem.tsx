8e533f5c1ef65b6fc289d86519929fdf93a83310
Oliver Henrichs <ohenrichs@gmail.com>
2026-01-27 23:51:58 +0100

    reduce padding of pattern list items

--- components/pattern/list/PatternListItem.tsx
+++ components/pattern/list/PatternListItem.tsx
@@ -88,7 +88,8 @@ const getStyles = (palette: Record<PaletteColor, string>) =>
   StyleSheet.create({
     patternItem: {
       paddingVertical: 4,
-      paddingHorizontal: 12,
+      paddingRight: 4,
+      paddingLeft: 12,
       borderRadius: 8,
       borderWidth: 2,
       marginBottom: 8,
